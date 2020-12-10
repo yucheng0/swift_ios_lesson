4a45b3beda05012bf629cfe46709e85596d2a0a9
MichaelTseng <yucheng0939@gmail.com>
2020-12-11 00:02:44 +0800

    Initial Commit

---
+++ swift_ios_callback/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_ios_callback
+//
+//  Created by yuchengtseng on 2020/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
